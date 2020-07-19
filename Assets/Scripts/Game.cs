f415f7195fb7e669e19f8d51984f14bbdd1289d3
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-07-18 18:24:15 -0700

    Added Truck

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject truckRoadPrefab;
+
+    void Start()
+    {
+        Instantiate(truckRoadPrefab,  truckRoadPrefab.GetComponent<Transform>().position, Quaternion.identity);    
+    }
+}
---
+++ Assets/Scripts/TrucksRoad.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class TrucksRoad : MonoBehaviour
+{
+    public GameObject truckPrefab;
+
+    void Start()
+    {
+        Instantiate(truckPrefab, truckPrefab.GetComponent<Transform>().position, Quaternion.identity);
+    }
+}
