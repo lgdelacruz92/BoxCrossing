cfb15815da236636ae0399abef9823cf15cf38b2
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-07-19 08:10:53 -0700

    Added Colors

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public GameObject truckRoadPrefab;
+    public RoadFactory roadFactory;
 
     void Start()
-    {
-        Instantiate(truckRoadPrefab,  truckRoadPrefab.GetComponent<Transform>().position, Quaternion.identity);    
+    {   
+        Color[] colors = new Color[]{Color.red, Color.gray, Color.green};
+        for (int i = 1; i <= 20; i++) {
+            roadFactory.CreateRoad(new Vector3(0, -5.5f, i * 2), colors[i % colors.Length]);
+        }
     }
 }
---
+++ Assets/Scripts/RoadObjects.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RoadObjects
+{
+    public RoadObjects(Vector3 velocity) {
+
+    }
+
+    public void UpdateStep() {
+
+    }
+}
