0e9221974b16e96d8291cb5d28a23268552e82da
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-07-19 08:34:46 -0700

    Game mechanics is started

---
+++ Assets/RoadObjectFactory.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class RoadObjectFactory : MonoBehaviour
+{
+    public GameObject roadObjectPrefab;
+
+    public GameObject CreateRoadObject(Vector3 pos, Vector3 velocity, Color color, Vector3 size) {
+        GameObject roadObject = Instantiate(roadObjectPrefab, pos, Quaternion.identity);
+        roadObject.GetComponent<MeshRenderer>().material.color = color;
+        roadObject.GetComponent<Transform>().localScale = size;
+        roadObject.GetComponent<Rigidbody>().velocity = velocity;
+        return roadObject;
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,11 +5,42 @@ public class Game : MonoBehaviour
 {
     public RoadFactory roadFactory;
 
+    public RoadObjectFactory roadObjectFactory;
+
+    private GameObject[] roads;
+
+    private GameObject[] roadObjects;
+
+    private int roadsSize;
+
     void Start()
     {   
+        roadsSize = 20;
+        roads = new GameObject[roadsSize];
+        roadObjects = new GameObject[roadsSize];
+
         Color[] colors = new Color[]{Color.red, Color.gray, Color.green};
-        for (int i = 1; i <= 20; i++) {
-            roadFactory.CreateRoad(new Vector3(0, -5.5f, i * 2), colors[i % colors.Length]);
+        for (int i = 0; i < roadsSize; i++) {
+            Vector3 pos = new Vector3(0, -5.5f, i * 2 + 2);
+            roads[i] = roadFactory.CreateRoad(pos, colors[i % colors.Length]);
+
+            Vector3 velocity = new Vector3(-10, 0, 0);
+            if (i % 2 == 1) {
+                velocity = new Vector3(10, 0, 0);
+            }
+            roadObjects[i] = roadObjectFactory.CreateRoadObject(pos + new Vector3(0, 7, 0), velocity, colors[i % colors.Length], new Vector3(10, 2, 1.5f));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < roadsSize; i++) {
+            if (roadObjects[i].GetComponent<Transform>().position.x < -50) {
+                roadObjects[i].GetComponent<Transform>().position += new Vector3(100, 0, 0);
+            }
+            else if (roadObjects[i].GetComponent<Transform>().position.x > 50) {
+                roadObjects[i].GetComponent<Transform>().position += new Vector3(-100, 0, 0);
+            }
         }
     }
 }
