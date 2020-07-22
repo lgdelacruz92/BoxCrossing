1adfa040c77bd372c645fc6588ccde859221242f
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-07-22 06:56:18 -0700

    Added Obstacle

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class Level1 : MonoBehaviour
 {
     public Text scoreFeedback;
-    public ObstacleFactory roadFactory;
+    public ObstacleFactory obstacleFactory;
 
     void Start()
     {
